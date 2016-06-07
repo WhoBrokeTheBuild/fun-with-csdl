ab3cad763dca52f649a4659cc242407385e187e2
Stephen Lane-Walsh <slanewalsh@mywebgrocer.com>
2016-06-07 16:03:40 -0400

    Added linetest-2 with boxes Fixed placement of linetest-1

--- src/main.c
+++ src/main.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void render_box(SDL_Renderer* rdr, float mX, float mY, float perc)
+{
+    float x1, x2, y1, y2;
+    x1 = mX * perc;
+    y1 = mY * perc;
+    x2 = 1024 - ( (1024 - mX) * perc );
+    y2 = 768 - ( (768 - mY) * perc );
+
+    SDL_RenderDrawLine(rdr, x1, y1, x2, y1); // Top-Left to Bot-Left
+    SDL_RenderDrawLine(rdr, x1, y1, x1, y2); // Top-Left to Top-Right
+    SDL_RenderDrawLine(rdr, x2, y1, x2, y2); // Bot-Left to Bot-Right
+    SDL_RenderDrawLine(rdr, x1, y2, x2, y2); // Top-Right to Bot-Left
+}
+
 int main(int argc, char** argv)
 {   
     SDL_Init(SDL_INIT_VIDEO);
@@ -52,35 +66,11 @@ int main(int argc, char** argv)
         }
 
         SDL_RenderClear(rdr);
-       
-        int stepCount = 20;
-        float step = mX / stepCount;
-        for (int i = 0; i < stepCount; ++i) 
-        {
-            SDL_RenderDrawLine(rdr, 0, 0,   mX - (i * step), mY);
-            SDL_RenderDrawLine(rdr, 0, 768, mX - (i * step), mY);
-        }
 
-        step = (1024 - mX) / stepCount;
-        for (int i = 0; i < stepCount; ++i)
-        {
-            SDL_RenderDrawLine(rdr, 1024, 0,   mX + (i * step), mY);
-            SDL_RenderDrawLine(rdr, 1024, 768, mX + (i * step), mY); 
-        }
-
-        step = mY / stepCount;
-        for (int i = 0; i < stepCount; ++i)
-        {
-            SDL_RenderDrawLine(rdr, 0,    0, mX, mY - (i * step));
-            SDL_RenderDrawLine(rdr, 1024, 0, mX, mY - (i * step)); 
-        }
-
-        step = (768 - mY) / stepCount;
-        for (int i = 0; i < stepCount; ++i)
-        {
-            SDL_RenderDrawLine(rdr, 0,    768, mX, mY + (i * step));
-            SDL_RenderDrawLine(rdr, 1024, 768, mX, mY + (i * step)); 
-        }
+        render_box(rdr, mX, mY, 0.2f);
+        render_box(rdr, mX, mY, 0.4f);
+        render_box(rdr, mX, mY, 0.6f);
+        render_box(rdr, mX, mY, 0.8f);
 
         SDL_RenderDrawLine(rdr, 0, 0, mX, mY);
         SDL_RenderDrawLine(rdr, 1024, 0, mX, mY);
